0c8a4b01198bcdab1a5237cec823e65d5f496126
francoalvarezderon <francoalvarezderon53@gmail.com>
2025-08-12 02:20:22 -0300

    solucion

--- eventos.js
+++ eventos.js
@@ -2,3 +2,9 @@ const div = document.getElementById("divUno");
  div.addEventListener("click", function() {
     alert("Hola! Soy el div");
 });
+const fueraDiv = document.getElementById("boton");
+fueraDiv.addEventListener("click", function(event) {
+    event.stopPropagation(); 
+    alert("Fuera la propagación del div");
+});
+
